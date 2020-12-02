a2317cf899185cb520ecbfbf2e618486d4c75939
AH-REM <35509872+AH-REM@users.noreply.github.com>
2020-12-02 13:42:51 +0100

    Add PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PostController extends Controller
+{
+    /**
+     * Renvoie la liste des postes
+     */
+    public function list() {
+        return 'Voici une liste !';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->get('/', function () {
+    return 'homepage';
 });
+
+$router->get('post', 'PostController@list');
