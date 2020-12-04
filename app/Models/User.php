2ff0fe02b3fed2a894a2d7ba176231188b6f480f
Mikail <8000henri@gmail.com>
2020-12-04 13:42:51 +0100

    Added users rules

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,4 +30,9 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     protected $hidden = [
         'password',
     ];
+
+    public static $rules = [
+        'username' => 'required|min:3|max:12|unique:users',
+        'password' => 'required|min:4|max:18',
+    ];
 }
