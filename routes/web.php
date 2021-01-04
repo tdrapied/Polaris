8e5e5111d558f749a7cfaa98f2c70d75cac0cfc3
AH-REM <35509872+AH-REM@users.noreply.github.com>
2021-01-04 15:26:10 +0100

    Add admin route

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ $router->group(['prefix' => 'signup'], function () use ($router) {
   * Post
   */
 
-$router->group(['prefix' => 'new'], function () use ($router) {
+$router->group(['prefix' => 'new', 'middleware' => 'auth'], function () use ($router) {
 
     $router->get('/', ['as' => 'post_new', 'uses' => 'PostController@form']);
     $router->post('/', 'PostController@form');
@@ -49,8 +49,18 @@ $router->group(['prefix' => 'edit/{id:[0-9]+}'], function () use ($router) {
 
 });
 
+$router->get('delete/{id:[0-9]+}', [ 'as' => 'post_delete', 'uses' => 'PostController@delete' ]);
+
 $router->get('search', ['as' => 'post_search', 'uses' => 'PostController@search']);
 
 $router->get('random', ['as' => 'post_random', 'uses' => 'PostController@random']);
 
-$router->get('delete/{id:[0-9]+}', [ 'as' => 'post_delete', 'uses' => 'PostController@delete' ]);
+/**
+ * Admin
+ */
+
+$router->group(['prefix' => 'admin'], function () use ($router) {
+
+    # $router->get('users', [ 'as' => 'users_list', 'uses' => 'PostController@list' ]);
+
+});
