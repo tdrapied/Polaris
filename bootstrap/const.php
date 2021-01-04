b1cb9311d95b499a670681521cca0979ef19c187
AH-REM <35509872+AH-REM@users.noreply.github.com>
2021-01-04 15:10:12 +0100

    Update const

--- bootstrap/const.php
+++ bootstrap/const.php
@@ -1,4 +1,4 @@
 <?php
-define('COOKIE_SESSION_KEY', 'polaris');
-define('COOKIE_SECRET', '');
+define('COOKIE_SESSION_KEY', 'polaris_session');
+define('COOKIE_SECRET', 'k3mr2mUJDhuQM9kALJMLkFtY4xGYMMRF9XZ7hZ9x7bUkm3x4Qr');
 define('COOKIE_CYPHER', 'aes-256-ctr');
