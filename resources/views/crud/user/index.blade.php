6d56ce1705f677f1cd98103917afb0eb39270227
Mikail <8000henri@gmail.com>
2021-01-05 14:42:10 +0100

    forms added for edit crud

--- resources/views/crud/user/index.blade.php
+++ resources/views/crud/user/index.blade.php
@@ -1,9 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
 <div class="panel panel-default">
         <table class="table table-bordered has-action">
             <thead>
                 <tr>
                     <th>Username</th>
                     <th>Password</th>
+                    <th>Role</th>
                     <th></th>
                 </tr>
             </thead>
@@ -12,6 +16,7 @@
                     <tr>
                         <td>{{ $user->username }}</td>
                         <td>{{ $user->password }}</td>
+                        <td>{{ $user->role }}</td>
                         <td>
                             <a href="{{ route('user_edit', ['id' => $user->id]) }}" class="btn btn-info btn-sm">Edit</a>
                         </td>
@@ -20,3 +25,4 @@
             </tbody>
         </table>
 </div>
+@endsection
