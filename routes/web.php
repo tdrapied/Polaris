1ed755ea5f721873046023a021d5693cd0142c87
Constantin <ticu369@gmail.com>
2021-01-05 14:21:13 +0100

    Add: page moderateur

--- routes/web.php
+++ routes/web.php
@@ -55,8 +55,6 @@ $router->group(['prefix' => 'edit/{id:[0-9]+}'], function () use ($router) {
 
 });
 
-$router->get('delete/{id:[0-9]+}', [ 'as' => 'post_delete', 'uses' => 'PostController@delete' ]);
-
 $router->get('search', ['as' => 'post_search', 'uses' => 'PostController@search']);
 
 $router->get('random', ['as' => 'post_random', 'uses' => 'PostController@random']);
@@ -68,11 +66,13 @@ $router->get('delete/{id:[0-9]+}', [ 'as' => 'post_delete', 'uses' => 'PostContr
  * Admin Post
  */
 
-$router->get('validation', ['as' => 'validation', 'uses' => 'AdminController@listAll']);
+$router->get('deleteAdmin/{id:[0-9]+}', [ 'as' => 'post_delete', 'uses' => 'AdminController@deleteAdmin' ]);
 
-$router->get('activate/{id:[0-9]+}', [ 'as' => 'post_activate', 'uses' => 'AdminController@activate' ]);
+$router->get('crud/user/admin', ['as' => 'validation', 'uses' => 'AdminController@listAll']);
 
-$router->get('deactivate/{id:[0-9]+}', [ 'as' => 'post_deactivate', 'uses' => 'AdminController@deactivate' ]);
+$router->get('crud/user/admin/activate/{id:[0-9]+}', [ 'as' => 'post_activate', 'uses' => 'AdminController@activate' ]);
+
+$router->get('crud/user/admin/deactivate/{id:[0-9]+}', [ 'as' => 'post_deactivate', 'uses' => 'AdminController@deactivate' ]);
 
 /**
  * CRUD_user
