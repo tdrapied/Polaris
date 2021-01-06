2b1f7df8ed4d352cb82b1053ce6e9e8c9c55fa6c
AH-REM <35509872+AH-REM@users.noreply.github.com>
2021-01-06 13:38:28 +0100

    Update views

--- resources/views/post/search.blade.php
+++ resources/views/post/search.blade.php
@@ -16,6 +16,7 @@
             <p class="lead mb-5">Aucun élément ne correspond à votre recherche.</p>
 
             <img class="rounded" height="280" src="https://media1.tenor.com/images/ed32319213bb232b3e9fb85cf06739d9/tenor.gif" alt="travolta confused">
+            
         </div>
 
     @endforelse
